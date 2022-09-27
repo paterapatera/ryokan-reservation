f4f4e912d61f698cd23677b2515cd0c3992c38d3
pater4 <pater4.1127+patera@gmail.com>
2022-09-28 05:16:51 +0900

    prettier対応

--- admin/app/Domains/Employee/Exception/DuplicateEmailException.php
+++ admin/app/Domains/Employee/Exception/DuplicateEmailException.php
@@ -4,13 +4,11 @@ namespace App\Domains\Employee\Exception;
 
 use App\Domains\Shared\DomainException;
 
-class DuplicateEmailException extends DomainException
-{
-    /**
-     * @param class-string $className
-     */
-    function __construct($className, int $code = 0, \Throwable|null $previous = null)
-    {
-        parent::__construct('Eメールが重複しています', $className, $code, $previous);
-    }
+class DuplicateEmailException extends DomainException {
+  /**
+   * @param class-string $className
+   */
+  function __construct($className, int $code = 0, \Throwable|null $previous = null) {
+    parent::__construct('Eメールが重複しています', $className, $code, $previous);
+  }
 }
